bb4ff8caa7ae70c99f053046778d507b31bdd1d7
Earendil-C <55442093+Earendil-C@users.noreply.github.com>
2022-07-27 00:11:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="media2237.xyz";
+        url.hostname="media2237.tk";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
